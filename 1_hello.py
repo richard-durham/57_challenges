cfe11f20247ab735bcccf3f61ed4d95f7184bae8
Richard Durham <richard_durham@me.com>
2016-01-05 13:42:22 +0000

    feat:  Add Exercise 1 and first challenge

---
+++ 1_hello.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+'''Excercises for Programmers
+	57 Challenges to Develop your Coding Skills
+	Excercise # 1
+
+	Create a program that prompts for your name
+		and prints a greeting using your name
+
+	Constraint: keep input, string concatination, and output seperate
+
+	Challenges: 
+		Write a new version of the program without variables
+
+		Write a version that displays different greetings for different people
+'''
+
+def get_name():
+	print "Hello, what is your name?"
+	name = raw_input(':')
+	return name
+
+
+def hello_name(name):
+	greeting = "Greetings, " + name + '!'
+	print greeting, "\n"
+
+# main program
+hello_name(get_name())
+
+
+# Challenge - same object but use NO variables
+print "Now without variables!  :) \n"
+
+print "Yo, what do they call you? %s", raw_input(':'), "?  Cool name, nice to meet you!"
